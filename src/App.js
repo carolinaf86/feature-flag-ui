bc3e94cc1fd38a13e6931e8aa5d358e5385f1627
cfuller <cfuller@overstock.com>
2022-02-01 08:55:04 +0000

    Clean up unused files, fix app title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import './App.css';
 import Form from './components/Form';
 import schema from './schema.json';
 import initialData from './initialData.json';
